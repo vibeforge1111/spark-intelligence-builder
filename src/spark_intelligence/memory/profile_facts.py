27fa33ae94ec877f47568ae7d6dd5e0e2ebb9b98
vibeforge1111 <vibeforge1111@gmail.com>
2026-04-10 16:49:10 +0400

    Support more natural startup and founder phrasings

--- src/spark_intelligence/memory/profile_facts.py
+++ src/spark_intelligence/memory/profile_facts.py
@@ -9,6 +9,33 @@ _CITY_PATTERNS = [
     re.compile(r"\bi\s+live\s+in\s+([a-z][a-z\s\-'`.]{1,40})", re.I),
     re.compile(r"\bi(?:'m| am)\s+in\s+([a-z][a-z\s\-'`.]{1,40})", re.I),
 ]
+_STARTUP_PATTERNS = [
+    re.compile(r"\bmy\s+startup\s+is\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+    re.compile(r"\b([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})\s+is\s+my\s+startup", re.I),
+    re.compile(r"\bi\s+created\s+a\s+startup\s+called\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+    re.compile(r"\bi\s+run\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+]
+_FOUNDER_PATTERNS = [
+    re.compile(r"\bi\s+founded\s+a\s+startup\s+called\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+    re.compile(r"\bi(?:'m| am)\s+the\s+founder\s+of\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+    re.compile(r"\bi\s+founded\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+    re.compile(r"\bi\s+started\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+    re.compile(r"\bi\s+built\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+    re.compile(r"\bi\s+launched\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+]
+_HACK_PATTERNS = [
+    re.compile(r"\bwe\s+were\s+hacked\s+by\s+([A-Za-z][A-Za-z0-9\s\-'`.&_]{1,60})", re.I),
+]
+_MISSION_PATTERNS = [
+    re.compile(r"\bi(?:'m| am)\s+trying\s+to\s+([A-Za-z][A-Za-z0-9\s\-'`.&,]{3,120})", re.I),
+    re.compile(r"\bi(?:'m| am)\s+(rebuilding\s+after\s+the\s+hack|reviving\s+the\s+companies)(?:[.!?,]|$)", re.I),
+]
+_SPARK_ROLE_PATTERNS = [
+    re.compile(r"\bspark\s+(?:is\s+going\s+to\s+be|will\s+be)\s+an\s+important\s+part\s+of\s+this(?:\s+rebuild)?", re.I),
+]
+_OCCUPATION_PATTERNS = [
+    re.compile(r"\bi(?:'m| am)\s+an\s+(entrepreneur)(?:\s+(?:now|today|currently))?(?:[.!?,]|$)", re.I),
+]
 _NAME_PATTERNS = [
     re.compile(r"\bmy\s+name\s+is\s+([a-z][a-z\s\-'.`]{0,40})", re.I),
     re.compile(r"\bcall\s+me\s+([a-z][a-z\s\-'.`]{0,40})", re.I),
@@ -16,6 +43,8 @@ _NAME_PATTERNS = [
 _COUNTRY_PATTERNS = [
     re.compile(r"\bmy\s+country\s+is\s+([a-z][a-z\s\-'`.]{1,40})", re.I),
     re.compile(r"\bi(?:'m| am)\s+from\s+([a-z][a-z\s\-'`.]{1,40})", re.I),
+    re.compile(r"\bi(?:'m| am)\s+based\s+in\s+([a-z][a-z\s\-'`.]{1,40})", re.I),
+    re.compile(r"\bi(?:'m| am)\s+based\s+out\s+of\s+([a-z][a-z\s\-'`.]{1,40})", re.I),
 ]
 _TIMEZONE_PATTERNS = [
     re.compile(r"\bmy\s+timezone\s+is\s+([A-Za-z_]+/[A-Za-z_]+(?:/[A-Za-z_]+)?)", re.I),
