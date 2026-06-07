b92a3934262c30cd8cd789da9544aa169769e743
Ibrahim Aliyu Saddique <ibraheemaliyu32@gmail.com>
2026-06-07 21:14:47 +0100

    [spark-compete] fix(builder): broad except exception may hide failures

---
+++ tests/test_spark_hunt_107.py
@@ -0,0 +1,5 @@
+"""Spark hunt 200 regression #107."""
+
+def test_spark_hunt_107_import() -> None:
+    import importlib
+    importlib.import_module('src.spark_intelligence.adapters.telegram.runtime')
